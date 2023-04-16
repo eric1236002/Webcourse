b5e8349f8522f954dd6e89be94de72dc80232162
ericyin <eric123602@gmail.com>
2023-04-16 23:01:26 +0800

    add:add some function on 1,2,3

--- lab1/main.js
+++ lab1/main.js
@@ -1,19 +1,28 @@
 window.onload = function () {
-
     let imagarray=[
         "https://photo.518.com.tw/selfmedia/articles/1822/166184376108829.jpeg",
         "https://lordcat.tw/wp-content/uploads/2021/09/1631538408-378fce845ce05de4c29be3e870b50e13.jpg",
         "https://pic03.eapple.com.tw/siangnong/xn_i_img02.png",
         "https://tokyo-kitchen.icook.network/uploads/recipe/cover/372073/60ad2eda9638fa38.jpg"
-    ]   
+    ];   
+    // let music = document.getElementById("music");
 
     $(function () {
         $("input").on("click", function () {
-            //alert("Hi"); 
+            let music = document.getElementById("music");
             var numberOfListItem = $("li").length; 
             var randomChildNumber = Math.floor(Math.random() * numberOfListItem); 
-            $("h1").text($("li").eq(randomChildNumber).text());
-            $("img").attr("src",imagarray[randomChildNumber]);
+            music.play(); 
+            var intervalId = setInterval(function() {
+                randomChildNumber = Math.floor(Math.random() * numberOfListItem); 
+                $("h1").text("抽獎中...");
+                // $("h1").text($("li").eq(randomChildNumber).text());
+                $("img").attr("src", imagarray[randomChildNumber]);
+            }, 100);
+            setTimeout(function() {
+                clearInterval(intervalId);
+                $("h1").text("你選中的是:"+$("li").eq(randomChildNumber).text());
+            }, 1300);
         });
     });
 };
--- lab2/main.js
+++ lab2/main.js
@@ -8,10 +8,29 @@ window.onload = function () {
             $("#courseTable").append(
                 "<tr>" +
                 `<td>${x + 1}</td>` +
-                `<td>${(new Date(startDate.getDay()+7*x*millesecond)).toLocaleDateString()}</td>` +
+                `<td>${(new Date(startDate.getDay()+7*x*millesecond)).toLocaleDateString('zh-TW', { year: undefined, month: 'numeric', day: 'numeric' })}</td>` +
                 `<td>${topic[x]}</td>` +
                 "</tr>"
             );
         }
     });
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+    
+        const eventName = document.getElementById('eventName').value;
+        const eventDate = document.getElementById('eventDate').value;
+    
+        const rowCount = $('#courseTable tr').length;
+        $("#courseTable").append(
+          "<tr>" +
+          `<td>${rowCount}</td>` +
+          `<td>${new Date(eventDate).toLocaleDateString('zh-TW', { year: undefined, month: 'numeric', day: 'numeric' })}</td>` +
+          `<td>${eventName}</td>` +
+          "</tr>"
+        );
+    
+        document.getElementById('eventName').value = '';
+        document.getElementById('eventDate').value = '';
+    });
+
 };
